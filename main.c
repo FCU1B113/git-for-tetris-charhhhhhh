092a7463bc8e1e6b239388dc6be97976f5cb24c3
charhhhhhh <charliewang1201@gmail.com>
2025-04-24 10:54:24 +0800

    new

---
+++ main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+typedef enum{
+    EMPTY=-1,
+    I,
+    J,
+    L,
+    O,
+    S,
+    T,
+    Z,
+}Shapeid;
+typedef struct 
+{
+Shapeid shape;
+Color color;
+};
+
+
+int main(){
+
+
+
+}
